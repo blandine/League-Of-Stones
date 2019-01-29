b88db688fca5d3bf75930bfa2cd5a0feb7dd16dc
blandine <blandine.descamps@gmail.com>
2019-01-29 15:13:08 +0100

    init cards collection

--- modules/cards.js
+++ modules/cards.js
@@ -1,3 +1,4 @@
+var champions = require('./champion.json');
 module.exports = {
   init: function(app, tools, losDB) {
     app.get('/cards/getAll', function(req, res) {
@@ -5,6 +6,11 @@ module.exports = {
         .collection('Cards')
         .find({})
         .toArray(function(err, result) {
+          if(!result || !result.length ) {
+            losDB.collection('Cards').insert(champions).then((inserted)=>{
+              tools.sendData(res, inserted, req, losDB, false);
+            })
+          }
           if (err != null) {
             tools.sendError(res, 'Error reaching mongo');
           } else {
